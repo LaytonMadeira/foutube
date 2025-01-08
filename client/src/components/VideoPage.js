a9808334b6c8529d7f7e3dc102cd5440ea82f871
Christian De La Torre <christiandelatorre@Christians-MacBook-Pro.local>
2025-01-08 16:01:52 -0500

    JavaScript

---
+++ client/src/components/VideoPage.js
@@ -0,0 +1,23 @@
+export default function MyApp() {
+    return (
+        <>
+        <a href="home.html">fakeYoutube</a><div id="VideoPlacement">
+            {/*insert using inner html */}
+            <img src="https://images.app.goo.gl/m1gf6yBGxsbS4tVr8" alt=""/>
+            </div>
+            <div id="Information">
+                <p>Views:</p>
+                <div id="Views">
+                    <p></p>
+                </div>
+                <p>Date Posted:</p>
+                <div id="Posted on">
+                    <p></p>
+                </div>
+                <div id="Description">
+                    <p></p>
+                </div>
+            </div>
+        </>
+    );
+  }
