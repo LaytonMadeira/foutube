519576764f434388c1b24c8ee43d33d3ab1d2368
Layton <layton.madeira@stonybrook.edu>
2025-01-17 17:11:44 -0500

    TESTING W FILE UPLOAD LOOKING GOOOOD

--- client/src/components/VideoPage.js
+++ client/src/components/VideoPage.js
@@ -1,10 +1,46 @@
-export default function MyApp() {
+import React, { useState } from "react";
+import axios from 'axios';
+
+export default function VideoPage() {
+    // create a state for uploading the video file , need state for uploading purpose
+    const [videoFile, setVideoFile] = useState(null);
+    // user adds a file 
+    let handleFileChange = (e) => {
+        // variable e represents our event object (button click to upload a file)
+        // change videoFile to now be our new file
+        setVideoFile(e.target.files[0]); 
+    };
+    // user hits upload button 
+    let handleUpload = async () => {
+        // check if valid video file exists 
+        if(!videoFile) {
+            alert("No Video File Was Chosen!");
+            return;
+        }
+        // we know we have some variable in the videoFile (maybe video maybe not TODO check)
+        
+        // create a new form , in which we will send the video to the backend for storage
+        const data = new FormData();
+        data.append('video', videoFile);
+
+        try{
+            console.log("Video File: ", videoFile);
+            const x = 40;
+            const resp = await axios.post('http://localhost:8000/upload/video', videoFile, x);
+        } catch(err){
+
+        }
+    };
     return (
         <>
         <a href="home.html">fakeYoutube</a><div id="VideoPlacement">
             {/*insert using inner html */}
             <img src="https://images.app.goo.gl/m1gf6yBGxsbS4tVr8" alt=""/>
             </div>
+            <input type="file" accept="video/*" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+
+            
             <div id="Information">
                 <p>Views:</p>
                 <div id="Views">
--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
+const multer = require("multer");
+// const GridFSStorage = require("multer-gridfs-storage");
+const gridfsStream = require("gridfs-stream");
+const GridFSStorage = require('multer-gridfs-storage').GridFsStorage;
+
+
 // middleware (security)
 // set up server to listen, using port 8000
 const server = app.listen(8000, () => {console.log("Server listening on port 8000...");});
@@ -18,24 +25,119 @@ mongoose.connect(mongoDB)
 //create variable for the connection (used by shutdown function)
 let mongooooseDb = mongoose.connection;
 
-// routes 
+const storage = multer.diskStorage({
+  destination: function( req, file, cb ) {
+    cb(null, 'test/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
 
+const upload = multer({ storage });
+
+app.post('/test/upload', upload.single('testFile'), (req, res) =>{
+  res.send("Did it work!?!");
+});
+
+
+// //setting up grid fs
+// let gfs;
+// const conn = mongooooseDb;
+// conn.once("open", () => {
+//   gfs = gridfsStream(conn.db, mongoose.mongo);
+//   gfs.collection("uploadsCOLLECTION"); // specify collection for GridFS
+// });
+
+// const storage =  new GridFSStorage({
+//   url: mongoDB,
+//   file: (req, file) => {
+//     console.log("Storage function executing!");
+//     console.log("File details: ", file);
+//     return;
+//     // return { 
+//       // bucketName: "uploadsCOLLECTION", // specify the collection name in GridFS
+//       // filename: file.originalname, // use original filename
+//     // };
+//   } 
+// });
+
+
+//to be used as a middleware to upload the files upon route request
+// const upload = multer({ storage });
+
+// middleware to allow requests from client
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+
+// middleware to simply log when a request is received
+app.use((req, res, next) => {
+    console.log("Request Received!");
+    next();
+});
+
+// routes 
+// route for when a user wants to upload a video
+// app.post("/upload/video", upload.single('videoFile'), async (req, res) => {
+//     console.log("Trying to Upload Video...");
+
+//     if( !req.file){
+//         console.log("There's no file here!");
+//     }
+//     else console.log("File uploaded!")
+    
+//     res.status(200).send("Done!");
+//     console.log("Done!");
+//     return;
+// }); 
 // end of routes
+
 
 //shuts down server when receiving SIGINT
 function shutdownFun(signal){
   // TODO DELETE?
   console.log(`Recieved ${signal}`);
   if(server){ 
-      server.close(()=> 
-      {
-          console.log("Server closed. Database instance disconnected");
-          if(mongooooseDb){
-              mongooooseDb.close().then(() => {
-                  console.log("la la la");
-                  process.exit(0);
-              });
-          }
-      })
+
+    const timeout = setTimeout(() => {
+        console.log("Forcefully shutting down due to timeout.");
+        process.exit(1);  // Exit forcefully if it takes too long
+      }, 10000); 
+
+    server.close(()=> 
+    {
+        clearTimeout(timeout);
+        console.log("Server closed.");
+        if(mongooooseDb){
+            mongooooseDb.close().then(() => {
+                console.log("Database instance disconnected");
+                console.log("la la la");
+                process.exit(0);
+            }).catch(err => {
+            console.log("We ran into an error when trying to close mongoose! ", err);
+            process.exit(1);
+            });
+        }
+        else{
+            console.log("No Mongoose");
+            process.exit(0);
+        }
+    });
+    // get all connections 
+    server.getConnections((err, count) => {
+        if (err) {
+          console.error("Error getting connections:", err);
+          process.exit(1);
+        }
+        console.log(`There are ${count} connections active.`);
+      });
+
+  }
+  else{
+    console.log("Server not open!");
+    process.exit(0);
   }
 }
